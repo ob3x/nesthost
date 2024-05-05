810ce5aca621debbacc770e31ecad50c649b9053
Szymon <ob3x.biznes@gmail.com>
2024-05-05 16:28:29 +0200

    update

---
+++ src/js/offers.js
@@ -0,0 +1,39 @@
+const minecrafts = document.querySelectorAll('.minecraft');
+const gtas = document.querySelectorAll('.gta');
+const discords = document.querySelectorAll('.discord');
+const others = document.querySelectorAll('.other');
+const menuBtns = document.querySelectorAll('.products-menu__item');
+const allItems = document.querySelectorAll('.products-box__item');
+
+const filterItems = (btn) => {
+	menuBtns.forEach((btn) => {
+		btn.classList.remove('products-menu__item--active');
+	});
+	allItems.forEach((item) => {
+		item.classList.add('active');
+	});
+	btn.classList.add('products-menu__item--active');
+	if (btn.value == 0) {
+		minecrafts.forEach((minecraft) => {
+			minecraft.classList.remove('active');
+		});
+	} else if (btn.value == 1) {
+		gtas.forEach((gta) => {
+			gta.classList.remove('active');
+		});
+	} else if (btn.value == 2) {
+		discords.forEach((discord) => {
+			discord.classList.remove('active');
+		});
+	} else if (btn.value == 3) {
+		others.forEach((other) => {
+			other.classList.remove('active');
+		});
+	}
+};
+
+menuBtns.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		filterItems(btn);
+	});
+});
